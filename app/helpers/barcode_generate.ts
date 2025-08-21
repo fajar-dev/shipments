0b6b7e78ab7e12a6d78ea5265980b714c7f5bcad
fajar-dev <fajarrivaldi2015@gmail.com>
2025-08-21 09:46:33 +0700

    init: barcode helpers

---
+++ app/helpers/barcode_generate.ts
@@ -0,0 +1,36 @@
+import bwipjs from 'bwip-js'
+
+export default class BarcodeGenerate {
+  /**
+   * Generate barcode PNG in Base64 format
+   * @param text Data to be converted into a barcode
+   * @param options Additional bwip-js options
+   * @returns string Base64 image
+   */
+  static async generateBase64(
+    text: string,
+    options?: {
+      bcid?: string
+      scale?: number
+      height?: number
+      includetext?: boolean
+      textxalign?: 'center' | 'left' | 'right'
+    }
+  ): Promise<string> {
+    try {
+      const png = await bwipjs.toBuffer({
+        bcid: options?.bcid || 'code128',
+        text: text,
+        scale: options?.scale || 10,
+        height: options?.height || 10,
+        includetext: options?.includetext ?? false,
+        textxalign: options?.textxalign || 'center',
+      })
+
+      return png.toString('base64')
+    } catch (err) {
+      console.error('Barcode generation error:', err)
+      throw err
+    }
+  }
+}
